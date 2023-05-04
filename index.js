cf09bacc5e54732bb69829cfebf494ec584c605f
Jay9874 <jayprakashsharma225@gmail.com>
2023-05-04 14:08:19 +0530

    delete server folder

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use("/api", auth);
 
 // Rest call response
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "./client", "build", "index.html"));
   });
 
 
